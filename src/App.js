b2e6e1277b4437213358775f815f4c5c740def63
dsevenx <50168945+dsevenx@users.noreply.github.com>
2022-05-23 20:52:09 +0200

    autoprefixer

--- src/App.js
+++ src/App.js
@@ -1,62 +1,107 @@
 import "./App.css";
 import React, { useState } from "react";
+import Tesseract from "tesseract.js";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [text, setText] = useState("");
   const [image, setImage] = useState("");
-  const [progress, setProgress] = useState(50);
+  const [imagename, setImagename] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  const onklickConvert = () => {
+    setIsLoading(true);
+    Tesseract.recognize(image, "deu", {
+      logger: (m) => {
+        console.log(m);
+        if (m.status === "recognizing text") {
+          setProgress(Math.round(m.progress * 100));
+        }
+      },
+    }).then(({ data: { text } }) => {
+      setText(text);
+      setIsLoading(false);
+    });
+  };
+
+  const onklickNewBild = () => {
+    setIsLoading(false);
+    setText("");
+    setImage("");
+    setImagename("");
+    setProgress(0);
+  };
 
   return (
     <div className="container" style={{ height: "100vh" }}>
       <div className="row h-100">
         <div className="col-md-5 mx-auto d-flex flex-column align-items-center">
-          {!isLoading && (
-            <h1 className="mt-5 mb-5">
-              Learn React by D7X Neu Image To Text (!isLoading)
-            </h1>
+          {!isLoading && !text && (
+            <h1 className="mt-5 mb-5">Bilderkennung by D7X - Bild wählen</h1>
           )}
 
-          {!isLoading && !text && (
+          {!isLoading && !text && !imagename && (
+            <div>
+              <div className="input-group-btn">
+                <label for="bildfile" className="btn btn-default">
+                  Bild auswählen
+                  <input
+                    id="bildfile"
+                    type="file"
+                    Style="display:none;"
+                    className="form-control btn mt-4"
+                    onChange={(event) => {
+                      setImage(URL.createObjectURL(event.target.files[0]));
+                      setImagename(
+                        "konvertiere Bild : " + event.target.files[0].name
+                      );
+                    }}
+                  />
+                </label>
+              </div>
+            </div>
+          )}
+
+          {!isLoading && !text && imagename && (
             <div>
               <input
-                type="file"
-                className="form-control"
-                onChange={(event) =>
-                  setImage(URL.createObjectURL(event.value.filege[0]))
-                }
-              />
-              <input
                 type="button"
+                disabled={image === ""}
                 className="form-control btn btn-primary mt-4"
-                value="konvertiere Bild"
+                value={imagename}
+                onClick={onklickConvert}
               />
             </div>
           )}
 
           {isLoading && (
             <div>
-              <h1>
-                Learn React by D7X New Image To Text (!isLoading) 111
-                Fortschrittsbalken
-              </h1>
-              <p className="text-center">
-                Fortschritt des Konvertierens: {progress} Prozent
-              </p>
+              <h1>Bilderkennung by D7X - Bild analysieren</h1>
+              <p className="text-center">Fortschritt : {progress} Prozent</p>
             </div>
           )}
 
           {!isLoading && text && (
             <div>
-              <h1>
-                Learn React by D7X New Image To Text (!isLoading) 222 text
-              </h1>
+              <h1>Bilderkennung by D7X - Text zum Bild</h1>
               <textarea
                 className="form-control"
                 rows="15"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
               ></textarea>
+            </div>
+          )}
+
+          {!isLoading && text && (
+            <div>
+              <input
+                type="button"
+                disabled={image === ""}
+                className="form-control btn btn-primary mt-4"
+                value="neues Bild"
+                onClick={onklickNewBild}
+              />
             </div>
           )}
         </div>
